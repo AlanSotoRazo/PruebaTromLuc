061e6f1ac0c6544d5e71547c85efc0d920e44927
Alan Soto <alansoto121298@gmail.com>
2025-08-15 13:27:25 -0600

    editar parte final del app.py

--- app.py
+++ app.py
@@ -284,4 +284,7 @@ def logout():
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
+    pass  # o coméntalo con #
+    # app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
+
+
